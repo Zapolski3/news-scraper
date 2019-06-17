48b7da3b5e78b62265c340fa31d3e913faead007
Pavel Zapolski <pavelzapolski@Pavels-MacBook-Pro.local>
2019-06-17 18:29:13 -0400

    added

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
